bc1b7c49cacd178be6c4acc9123ca460f7beced1
neng-li <1830103604@qq.com>
2020-12-09 13:01:54 +0800

    实验四

---
+++ src/main/java/com/example/demo/enity/UserRole.java
@@ -0,0 +1,27 @@
+package com.example.demo.enity;
+
+public class UserRole {
+
+
+	private Integer id;
+	private Integer uid;
+	public Integer getUid() {
+		return uid;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getRid() {
+		return rid;
+	}
+	public void setRid(Integer rid) {
+		this.rid = rid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	private Integer rid;
+}
